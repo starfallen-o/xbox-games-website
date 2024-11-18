9a03266055f507d352e7362029b93c1fcc958645
Orion Starfallen <Starfallen-O@ulster.ac.uk>
2024-11-18 17:09:48 +0000

    Changed port back

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
     return render_template('/html/index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 4999))
     app.run(debug=True, host='0.0.0.0', port=port)
