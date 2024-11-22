25d1128d24e201bf4610d0d3031e70e188c19f67
Orion Starfallen <Starfallen-O@ulster.ac.uk>
2024-11-22 17:53:00 +0000

    Changed host

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
