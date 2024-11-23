b74567e2ec17c0716520b166283ed55668aa8996
Orion Starfallen <Starfallen-O@ulster.ac.uk>
2024-11-23 18:41:18 +0000

    Secret Access test

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 import os
+import config
 
 app = Flask(__name__)
 
+secret_value = config.secret_value
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('/html/index.html')
+    return render_template('/html/index.html', name=secret_value)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
---
+++ config.py
@@ -0,0 +1,12 @@
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+# Key Vault
+KEY_VAULT_URI = f"https://b00858494keyvault.vault.azure.net/"
+
+# Initialise Azure Key Vault Client
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KEY_VAULT_URI, credential=credential)
+
+secret = client.get_secret("testsecret")
+secret_value = secret.value
